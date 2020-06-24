cdc6e1b525b58b53fc028295608ce674db329b11
Baron <popo333555@gmail.com>
2020-06-24 20:29:18 +0200

    add code

---
+++ driving.py
@@ -0,0 +1,15 @@
+country = input('What is your nationality: ')
+age = input('Enter your age: ')
+age = int(age)
+if country == 'Taiwan ':
+	if age >= 18:
+		print('Bro, go get the license. ')
+	else:
+		print('Sorry, you are too young for the drive license. ')
+elif country == 'USA':
+	if age >= 16:
+		print('Bro, go get the license. ')
+	else:
+		print('Sorry, you are too young for the drive license. ')
+else:
+	print('Sorry man')
