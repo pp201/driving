e62e631227b3656977a4366dd0296099f1970b1a
Baron <popo333555@gmail.com>
2020-06-24 20:37:19 +0200

    modify USA

--- driving.py
+++ driving.py
@@ -10,6 +10,6 @@ elif country == 'USA':
 	if age >= 16:
 		print('Bro, go get the license. ')
 	else:
-		print('Sorry, you are too young for the drive license. ')
+		print('Sorry, you are too young for the driver\'s license. ')
 else:
 	print('Sorry man')
